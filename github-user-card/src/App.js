715b0ac9fe9739c530f3ae6cc97ea9ae30f8f276
Robert Kulp <pedalinon@gmail.com>
2019-10-05 11:13:53 -0700

    Getting github user API

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Something else will go here</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			user: ''
+		}
+	}
+
+	componentDidMount() {
+
+		axios 
+			.get('https://api.github.com/users/rabithole/followers')
+			.then(res => {
+				this.setState(() => ({ user: res.data }));
+				console.log(this.state)
+			})
+			.catch(error => {
+				console.error('Server Error', error);
+			});
+	}
+
+	render() {
+	  return (
+	    <div className="App">
+	      <header className="App-header">
+	       <h1>Something else will go here</h1>
+	      </header>
+	    </div>
+	  );
+	}
 }
 
 export default App;
